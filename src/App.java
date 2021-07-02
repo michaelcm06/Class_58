64f0cf6a474ae1f841cac30d145c5d0d733050ff
michaelcm06 <castro520@hotmail.es>
2021-07-01 20:46:27 -0500

    Second class, java coding

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        int A=0;
+        int B=0;
+        int suma=0;
+
+        System.out.print("\nPor favor, introduzca el valor de A:\t ");
+        A=Integer.parseInt(System.console().readLine());
+        System.out.print("\nPor favor, introduzca el valor de B:\t10");
+        B=Integer.parseInt(System.console().readLine());
+
+        suma=A+B;
+        System.out.println(suma);
     }
 }
